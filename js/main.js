d068fefdf485693a251e5c0450cfea78850adbc8
sobbaka <a.bagaev1989@gmail.com>
2023-05-26 18:05:41 +0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -4,23 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const connect = document.querySelector('.connect');
   const connectLinks = document.querySelector(".connect__links");
 
-
-
-  if (screen.width > 650) {
-    connect.style.left = header.offsetLeft + 10 + 'px';
-    connect.style.top = (subtitle.offsetTop + subtitle.offsetHeight) * 1.1 + 'px';
-  }
-
+  alignButton()
   connect.classList.remove("none");
 
+  window.addEventListener('resize', () => {
+    let fixBtnAlign;
+    clearTimeout(fixBtnAlign);
+    fixBtnAlign = setTimeout(function () {
+      alignButton();
+    }, 100);
+  })
 
 
+  function alignButton() {
+
+
+    if (screen.width > 650) {
+      connect.style.top = (subtitle.offsetTop + subtitle.offsetHeight) * 1.1 + 'px';
+      connect.style.left = screen.width > 1280 ? header.offsetLeft + 'px' : header.offsetLeft + 10 + 'px';
+    } else {
+      connect.style.left = 0;
+    }
+  }
 
   connect.addEventListener("click", () => {
     connectLinks.classList.toggle("connect__links_hidden");
   })
-
-
 
 
   const about = document.querySelector('.about');
@@ -34,35 +43,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (headerPosition >= aboutPosition - 78) {
       header.classList.add("header__state_dark");
-      //header.style.backgroundColor = 'rgba(54, 53, 53, 0.418)';
+
     } else {
       header.classList.remove("header__state_dark");
-      //header.style.backgroundColor = 'transparent';
     }
 
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
       header.classList.remove("header__state_hidden");
-      //header.style.top = "25px";
     } else {
       header.classList.add("header__state_hidden");
-      //header.style.top = "-80px";
     }
     prevScrollpos = currentScrollPos;
   });
 
 
-
-  // let prevScrollpos = window.pageYOffset;
-  // window.onscroll = function () {
-  //   let currentScrollPos = window.pageYOffset;
-  //   if (prevScrollpos > currentScrollPos) {
-  //     header.style.top = "25px";
-  //   } else {
-  //     header.style.top = "-80px";
-  //   }
-  //   prevScrollpos = currentScrollPos;
-  // }
 });
 
 
