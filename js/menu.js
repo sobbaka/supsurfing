03189f5ebd11bb82510f9ae95772c8b13ce3a35a
sobbaka <a.bagaev1989@gmail.com>
2023-05-27 13:41:02 +0400

    fix mobile menu on click links

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector(".header__burger");
   const controls = document.querySelector(".header__controls");
 
+  const links = document.querySelectorAll(".navigation__link");
+
   menu.addEventListener("click", () => {
     menu.classList.toggle("header__burger_active");
     header.classList.toggle("header__state_active");
     logo.classList.toggle("none");
     controls.classList.toggle("header__controls_hidden");
+  })
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.toggle("header__burger_active");
+      header.classList.toggle("header__state_active");
+      logo.classList.toggle("none");
+      controls.classList.toggle("header__controls_hidden");
+
+      document.querySelector(".menuToggle__input").checked = false;
+    })
   })
 })
