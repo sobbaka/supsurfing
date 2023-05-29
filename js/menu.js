4783b8972e12340b2f7fb6e17151c4a162534794
sobbaka <a.bagaev1989@gmail.com>
2023-05-29 21:38:27 +0400

    fix prevent mobile menu action on screen over 950

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+
   const header = document.querySelector(".header");
   const logo = document.querySelector(".header__logo");
   const menu = document.querySelector(".header__burger");
@@ -16,12 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   links.forEach(link => {
     link.addEventListener("click", () => {
-      menu.classList.toggle("header__burger_active");
-      header.classList.toggle("header__state_active");
-      logo.classList.toggle("none");
-      controls.classList.toggle("header__controls_hidden");
 
-      document.querySelector(".menuToggle__input").checked = false;
+      if (window.innerWidth < 950) {
+        menu.classList.toggle("header__burger_active");
+        header.classList.toggle("header__state_active");
+        logo.classList.toggle("none");
+        controls.classList.toggle("header__controls_hidden");
+        document.querySelector(".menuToggle__input").checked = false;
+      }
+
     })
   })
 })
