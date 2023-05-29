43ea8be4f598231727e1f6e1e28cf858266dcdd4
sobbaka <a.bagaev1989@gmail.com>
2023-05-29 22:06:50 +0400

    prevent body when mobile menu is open

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+  const body = document.querySelector("body");
 
   const header = document.querySelector(".header");
   const logo = document.querySelector(".header__logo");
@@ -14,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     header.classList.toggle("header__state_active");
     logo.classList.toggle("none");
     controls.classList.toggle("header__controls_hidden");
+    body.classList.toggle("body__overflow_hidden");
   })
 
   links.forEach(link => {
@@ -25,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         logo.classList.toggle("none");
         controls.classList.toggle("header__controls_hidden");
         document.querySelector(".menuToggle__input").checked = false;
+        body.classList.toggle("body__overflow_hidden");
       }
 
     })
